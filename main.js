8356f693f91577885c182ce4c0fba8b339cb21e9
ShaylaWhite <shayladwhite1@gmail.com>
2021-03-03 13:58:23 -0800

    added words

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 const highscoreDisplay = document.querySelector('#highscore');
+
+const words = [
+    'Por favor',
+    'Yo no comprendo',
+    'Te quiero',
+    'Buenas tardes',
+    'Bella',
+    'Bailando',
+    'Mi Gente',
+    'Clandestino',
+    'No Me Acuerdo ',
+    'Gasolina',
+    'Lo siento',
+    'la boca',
+    'Leer',
+    'los pies',
+    'Hasta luego',
+    'Semana',
+    'Antes',
+    'Aunque',
+    'Poco',
+    'Bajo',
+    'Muy',
+    'Mucho',
+    'Encontrar',
+    'Trabajar',
+    'Pensar'
+  ];
