29c54c927aead9d0286b57295ec13f80be0148cc
ShaylaWhite <shayladwhite1@gmail.com>
2021-03-03 13:45:12 -0800

    added globals and consts

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+// Globals 
 let time = 10;
-let score =0; 
+let score = 0; 
+let isPlaying;
+
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const highscoreDisplay = document.querySelector('#highscore');
