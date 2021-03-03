f2922c91e78284c1f2f4c877c3566e7d65aa3f50
ShaylaWhite <shayladwhite1@gmail.com>
2021-03-03 15:21:34 -0800

    added output function for word

--- main.js
+++ main.js
@@ -53,9 +53,14 @@ const words = [
 
     //Pick random to display on page
     //takes in the array 'words'
-    //The Math.floor() function returns the largest integer less than or equal to a given number.
-    //Math.random() used with Math.floor() can be used to return random integers.
+    
     function showWord (words) {
+
+        //The Math.floor() function returns the largest integer less than or equal to a given number.
+        //Math.random() used with Math.floor() can be used to return random integers.
         const randIndex = Math.floor(Math.random) * words.length);
 
+        //output random word here 'h2'
+         currentWord.innerHTML = words[randIndex]
+        
     }
